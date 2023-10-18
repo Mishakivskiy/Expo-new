167b8d8509f2afacfdb8254ab91972e3830b3ad3
Ivan Mishakivskiy <91463513+Mishakivskiy@users.noreply.github.com>
2023-10-18 13:16:53 +0300

    add ids

--- components/Screen.js
+++ components/Screen.js
@@ -76,27 +76,28 @@ const Screen = () => {
             <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff', margin: 0.5, fontSize: 25, color: '#000', padding: 5 }}>
                 {todo.isEditing ? (
                     <>
-                    <TouchableOpacity onPress={() => handleSaveEdit(index)}>
+                    <TouchableOpacity onPress={() => handleSaveEdit(index)} key="save-edit">
                         <MaterialIcons name="done" size={24} color="green" />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleCancelEdit(index)}>
+                    <TouchableOpacity onPress={() => handleCancelEdit(index)} key="cancel-edit">
                         <MaterialIcons name="cancel" size={24} color="#e04400" />
                     </TouchableOpacity>
                     <TextInput
                     style={{ color: todo.isCompleted ? 'gray' : '#000', fontSize: 22, textDecorationLine: todo.isCompleted ? 'line-through' : 'none', borderLeftWidth: 1, borderLeftColor: 'red', padding: 5, backgroundColor: '#fff', height: 35, fontSize: 20, opacity: 0.8, flex: 1}}
                     value={editedText}
                     onChangeText={(text) => setEditedText(text)}
+                    key="input-edit"
                     />
                     </>
                 ) : (
                     <View style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: 15, flexWrap: 'wrap', justifyContent: 'space-between' }}>
-                    <TouchableOpacity onPress={() => handleToggleCompletion(index)}>
+                    <TouchableOpacity onPress={() => handleToggleCompletion(index)} key="complate-butt">
                         <MaterialIcons name="done" size={24} color={todo.isCompleted ? "#437a49" : "grey" }/>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleStartEdit(index)}>
+                    <TouchableOpacity onPress={() => handleStartEdit(index)} key="edit-butt">
                         <MaterialIcons name="edit" size={24} color="#42b0f5" />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleDeleteTodo(index)}>
+                    <TouchableOpacity onPress={() => handleDeleteTodo(index)} key="del-butt">
                         <MaterialIcons name="close" size={24} color="#e04400" />
                     </TouchableOpacity>
                     <Text style={{ color: todo.isCompleted ? 'gray' : '#000', fontSize: 22, textDecorationLine: todo.isCompleted ? 'line-through' : 'none', borderLeftWidth: 1, borderLeftColor: 'red', padding: 5, backgroundColor: '#fff', height: 35, fontSize: 20, opacity: 0.8, flex: 1 }}>
@@ -123,7 +124,7 @@ const Screen = () => {
             <View style={{ backgroundColor: 'white', borderTopWidth: 10, borderTopColor: 'brown', padding: 10 }}>
                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                     {todos.length > 0 && (
-                    <TouchableOpacity onPress={handleToggleAll}>
+                    <TouchableOpacity onPress={handleToggleAll} key="complate-all-butt">
                         <MaterialIcons
                         name='check-box'
                         size={30}
@@ -138,6 +139,7 @@ const Screen = () => {
                     value={todoText}
                     onChangeText={(text) => setTodoText(text)}
                     onSubmitEditing={handleAddTodo}
+                    key="new-todo-input"
                     />
                 </View>
             </View>
@@ -151,18 +153,18 @@ const Screen = () => {
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 10 }}>
                 <Text style={{ fontSize: 12, fontWeight: 'bold', color: '#83756f' }}>{countActiveTodos()} items left</Text>
                 <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity onPress={() => setCurrentFilter('All')}>
+                    <TouchableOpacity onPress={() => setCurrentFilter('All')} key="view-all-butt">
                         <Text style={{ fontSize: 12, color: '#83756f', fontWeight: currentFilter === 'All' ? 'bold' : 'normal', marginRight: 5 }}>All</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setCurrentFilter('Active')}>
+                    <TouchableOpacity onPress={() => setCurrentFilter('Active')} key="view-active-butt">
                         <Text style={{ fontSize: 12, color: '#83756f', fontWeight: currentFilter === 'Active' ? 'bold' : 'normal', marginRight: 5 }}>Active</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setCurrentFilter('Completed')}>
+                    <TouchableOpacity onPress={() => setCurrentFilter('Completed')} key="view-complated-butt">
                         <Text style={{ fontSize: 12, color: '#83756f', fontWeight: currentFilter === 'Completed' ? 'bold' : 'normal' }}>Completed</Text>
                     </TouchableOpacity>
                 </View>
                 {todos.some((todo) => todo.isCompleted) && (
-                    <TouchableOpacity onPress={clearCompleted}>
+                    <TouchableOpacity onPress={clearCompleted} key="clear-all-complated-butt">
                         <Text style={{ fontSize: 12, color: 'red', backgroundColor: 'rgba(0, 0, 0, 0.1)', color: '#777'}}>Clear Completed</Text>
                     </TouchableOpacity>
                 )}
